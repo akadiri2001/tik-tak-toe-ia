160cf4f97422d2ac273810a01489fde70ffba8fa
akadiri2001 <kadiri.anas0000@gmail.com>
2025-10-03 14:21:38 +0200

    Create ai.h

---
+++ ai.h
@@ -0,0 +1,34 @@
+#ifndef IA_H
+#define IA_H
+
+#include "jeu.h"
+
+#define MAX_ETATS 20000
+#define TAUX_APPRENTISSAGE 0.1
+#define TAUX_EXPLORATION 0.1
+
+typedef struct {
+    unsigned long long etat;
+    int action;
+    float valeur;
+} EtatAction;
+
+typedef struct {
+    int coup;
+    unsigned long long etat;
+} Historique;
+
+typedef struct {
+    EtatAction donnees[MAX_ETATS];
+    int nombre;
+    Historique historique[TAILLE_PLATEAU];
+    int taille_historique;
+} IA;
+
+void initialiser_ia(IA *ia);
+void charger_ia(IA *ia);
+void sauvegarder_ia(const IA *ia);
+int ia_choisir_coup(IA *ia, const Jeu *jeu, Case ia_joueur);
+void ia_apprendre(IA *ia, int victoire);
+
+#endif
